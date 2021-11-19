185b60678129ee50547df3fdaf759c3d217d9134
RyuujiAkira <jrabajac@gmail.com>
2021-11-19 10:42:25 +0000

    Added initial files

---
+++ src/main/java/com/qa/DropList/DropListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.DropList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/DropList/DropListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.DropList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
